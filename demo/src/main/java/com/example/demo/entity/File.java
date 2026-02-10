71a02889449adbb75d8b532eb4a9a40aff22f4e3
ChaMH00 <chamh2000@naver.com>
2026-02-10 15:40:13 +0900

    spring jpa

--- demo/src/main/java/com/example/demo/entity/File.java
+++ demo/src/main/java/com/example/demo/entity/File.java
@@ -1,27 +1,30 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import lombok.*;
+import jakarta.persistence.*;
 
 @Entity
 @Getter
 @Setter
-@ToString
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
-public class File extends TimeBase{
+public class File extends TimeBase {
+
     @Id
     private String uuid;
+
     @Column(name = "save_dir", nullable = false)
     private String saveDir;
+
     @Column(name = "file_name", nullable = false)
     private String fileName;
-    @Column(name = "file_type", nullable = false, columnDefinition = "int default 0")
+
+    @Column(name = "file_type", nullable = false)
     private int fileType;
+
     private long bno;
+
     @Column(name = "file_size")
     private long fileSize;
 }
